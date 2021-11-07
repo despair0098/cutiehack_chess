d0b0ad385257a39379a5c5d189549162b29c8e50
despair0098 <zijunwang324@gmail.com>
2021-11-06 17:26:45 -0700

    Added Bishop.cpp

---
+++ Bishop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Piece.cpp"
+
+class Bishop : public Piece {
+    Bishop(char color, int x1, int y1): Piece(color, x1, y1){};
+    char getChar(){return 'B';}
+
+    bool move(Piece& p2){
+        if(abs(p2.getX() - getX()) == abs(p2.getY() - getY())){
+            int xIncrement = (p2.getX() - getX()) / abs(p2.getX() - getX());
+            int yIncrement = (p2.getY() - getY()) / abs(p2.getY() - getY());
+
+        } else {
+            return false;
+        }
+    }
+
+};
